13a36436773cc3597b076f125b57a738c1b25a6b
Cupcakerainbow <Offendasmanyasucan@gmail.com>
2017-10-31 21:30:27 +0530

    Update bestmove.c

--- bestmove.c
+++ bestmove.c
@@ -19,6 +19,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -65,7 +66,7 @@ void loadOpeningBook(char* filename){
 position* getOpeningFromBook(position* initialPos, int plyDepth){
 	move ret; ret.move = 0;
 	move possibleMoves[100]; int l_posmoves = 0;
-	for (int i = 0; i < 50; ++i){
+	for (int i = 0; i < 50; ++i){ 
 		move* openingLine = openingBookMoves[i];
 		bool discard = false;
 		for (int j = 0; j<initialPos->moveno-1; ++j){
